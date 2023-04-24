765338f8991ef02651e6f923b85b0cb9ba716396
Proofzhe <proofzhe@gmail.com>
2023-04-24 13:58:40 +0800

    update route

--- Laravel_API/routes/api.php
+++ Laravel_API/routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\OrganizeActivityController;
 use App\Http\Controllers\MemberController;
 use App\Http\Controllers\ActivityController;
+use App\Http\Controllers\MessageController;
 
 
 /*
@@ -22,10 +23,10 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/eventdata',[EventController::class,'index']);
-
 Route::get('/OrganizeActivity',[OrganizeActivityController::class,'index']);
 
 Route::put('/members/update', [MemberController::class,'update']);
 
 Route::get('/activities', [ActivityController::class,'index']);
+
+Route::post('/messages',[MessageController::class,'store']);
--- Laravel_API/routes/web.php
+++ Laravel_API/routes/web.php
@@ -19,4 +19,3 @@ Route::get('/', function () {
 
 require __DIR__.'/auth.php';
 
-Route::post('/messages',[MessageController::class,'store']);
