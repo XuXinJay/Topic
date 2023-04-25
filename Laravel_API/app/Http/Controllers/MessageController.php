c88c0a400b30a476f1c954350cb91f015219c02b
Proofzhe <proofzhe@gmail.com>
2023-04-25 11:18:26 +0800

    message get

--- Laravel_API/app/Http/Controllers/MessageController.php
+++ Laravel_API/app/Http/Controllers/MessageController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Message;
+use Illuminate\Support\Facades\DB;
 
 class MessageController extends Controller
 {
@@ -21,6 +22,13 @@ class MessageController extends Controller
 
         // 返回回應或其他操作
         return ($request);
+    }
+
+    public function index(Request $request)
+    {
+        $allnews = DB::table('message')->get();
+        $json = $allnews->toJson();
+        return $json;
     }
 
     
