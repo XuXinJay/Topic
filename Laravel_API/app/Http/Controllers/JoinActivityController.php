3681c35959016cc4a5652acb879f303df738d770
weiting <mmm328520@gmail.com>
2023-04-27 14:45:54 +0800

    update joinActivities not yet finished

--- Laravel_API/app/Http/Controllers/JoinActivityController.php
+++ Laravel_API/app/Http/Controllers/JoinActivityController.php
@@ -16,33 +16,46 @@ class JoinActivityController extends Controller
             ->select('activities.*', 'users.*')
             ->get();
 
-        
-        return response()->json($activities,200,[],JSON_UNESCAPED_UNICODE);
+
+        return response()->json($activities, 200, [], JSON_UNESCAPED_UNICODE);
     }
-    
-    
-    
-    
+
     public function joinActivities()
     {
         $activities = joinActivities::join('users', 'users.id', '=', 'join_activities.member_id')
             ->join('activities', 'activities.activity_id', '=', 'join_activities.activity_id')
-            ->select('activities.*', 'users.*','join_activities.join_state')
+            ->select('activities.*', 'users.*', 'join_activities.join_state')
             ->get();
 
-        
-        return response()->json($activities,200,[],JSON_UNESCAPED_UNICODE);
+
+        return response()->json($activities, 200, [], JSON_UNESCAPED_UNICODE);
     }
+    public function update(Request $request)
+    {
+
+        $userId = $request->input('member_id');
+        $joinState = $request->input('join_state');
+        $user = joinActivities::where('member_id', $userId)->first();
+        if (!$user) {
+            return response()->json(["error" => "找不到對應的使用者"], 404);
+        }
+
+        $user->join_state = $joinState;
+        $user->save();
+
+        return response()->json(["message" => "已更新審查狀態"]);
+    }
+
     public function reviewActivities($activity_id)
     {
         $activities = joinActivities::join('users', 'users.id', '=', 'join_activities.member_id')
             ->join('activities', 'activities.activity_id', '=', 'join_activities.activity_id')
-            ->select('activities.*', 'users.*','join_activities.join_state')
-            ->where('join_activities.activity_id',$activity_id)
+            ->select('activities.*', 'users.*', 'join_activities.join_state')
+            ->where('join_activities.activity_id', $activity_id)
             ->get();
 
-        
-        return response()->json($activities,200,[],JSON_UNESCAPED_UNICODE);
+
+        return response()->json($activities, 200, [], JSON_UNESCAPED_UNICODE);
     }
 
 
@@ -56,8 +69,8 @@ class JoinActivityController extends Controller
             ->select('activities.*', 'users.*')
             ->get();
 
-        
-        return response()->json($activities,200,[],JSON_UNESCAPED_UNICODE);
+
+        return response()->json($activities, 200, [], JSON_UNESCAPED_UNICODE);
     }
 
 }
--- React-vite/src/event/EventReview.jsx
+++ React-vite/src/event/EventReview.jsx
@@ -10,6 +10,7 @@ function EventReview() {
   const { user, loading } = useAuthContext();
   const [joinActivities, setJoinActivities] = useState([]);
   const [memberInfo, setMemberInfo] = useState(user);
+  // const [join_activities, setjoinActivities] = useState([]);
   const { activity_id } = useParams();
 
   useEffect(() => {
@@ -34,6 +35,81 @@ function EventReview() {
 
 
 
+  // ------------------------------------------------------------------確認按鈕
+
+
+  useEffect(() => {
+    setMemberInfo(user);
+  }, [user]);
+  async function handleCheckPass(id) {
+    try {
+      const updateJoinState = {
+        ...joinActivities,
+        join_state: "已通過",
+      };
+      const res = await axios.post("/api/joinActivities/update", updateJoinState, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      console.log(res.data);
+      // window.location.href = `/review/${activity_id}`;
+    } catch (err) {
+      console.log(err.response.data);
+    }
+  }
+  handleCheckPass();
+
+
+
+
+
+
+
+
+
+
+
+  // ------------------------------------------------------------------拒絕按鈕
+
+
+  // useEffect(() => {
+  //   async function handleCheckPass() {
+  //     try {
+  //       const updateJoinState ={
+  //         ...memberInfo,
+  //         join_state:"已拒絕",
+  //       };
+  //       const res = await axios.put("/api/joinActivities", updateJoinState, {
+  //         headers: {
+  //           'Content-Type': 'application/json',
+  //         },
+  //       });
+  //       console.log(res.data);
+  //       window.location.href = '/member';
+  //     } catch (err) {
+  //       console.log(err.response.data);
+  //     }
+  //   }
+  //   getActivity();
+  // }, []);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -52,7 +128,7 @@ function EventReview() {
       {
         //  console.log(join_activities);
         joinActivities.map((activity) => {
-          if (user.id !== activity.id) {
+          if (user.id !== activity.id && activity.join_state === "審核中") {
             return (
               <React.Fragment>
 
@@ -73,7 +149,7 @@ function EventReview() {
                     </div>
 
                     <div className="event_page-check">
-                      <button className="event_page-check_pass">確認</button>
+                      <input type="button" defaultValue="確認" className="event_page-check_pass" onClick={handleCheckPass(activity.activity_id)} />
                       <button className="event_page-check_refuse">拒絕</button>
                     </div>
                   </div>
