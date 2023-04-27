793f358dc26772794a2dbbfef5bed15438c54ff9
XuXinJay <vv19950429@gmail.com>
2023-04-27 18:08:53 +0800

    update

--- React-vite/src/pages/EventReviewPages.jsx
+++ React-vite/src/pages/EventReviewPages.jsx
@@ -7,7 +7,7 @@ function EventReviewPages() {
   return (
     <div>
       <Header />
-      <div className="">
+      <div className="activity_all_box">
         <EventReview />
       </div>
       <Footer />
