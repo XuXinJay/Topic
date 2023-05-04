f84c5e39dd508300d93ebdfe048fc581043386d7
Proofzhe <proofzhe@gmail.com>
2023-05-04 14:14:28 +0800

    like

--- Laravel_API/app/Models/favoriteActivities.php
+++ Laravel_API/app/Models/favoriteActivities.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class favoriteActivities extends Model
 {
     protected $table = 'favorite_activities';
+    protected $fillable = [
+        'member_id',
+        'activity_id _id',
+        
+    ];
 }
